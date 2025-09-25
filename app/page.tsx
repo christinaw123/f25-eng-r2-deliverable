b3bc25e64d16b21bc195d6ccba717cdf82496d84
Christina Wong <cwong@college.harvard.edu>
2025-09-25 17:05:39 -0400

    homepage fix

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <TypographyH2>
-        Welcome to T4SG <span className="text-green-400">Biodiversity Hub</span>!
+        Welcome to <span className="text-green-400">Biodiversity Hub</span>!
       </TypographyH2>
       <TypographyP>
         Biodiversity Hub is a web-app that allows users to post information about different species and stay educated on
@@ -14,10 +14,6 @@ export default function Home() {
       </TypographyP>
       <TypographyP>To see the species page, log in in the top right!</TypographyP>
       <Separator className="my-4" />
-      <TypographyP>
-        Biodiversity Hub was created as an example webapp for T4SG&apos;s Spring 2024 applications.
-      </TypographyP>
-      <TypographyP>Good luck! We can&apos;t wait to see what you create :)</TypographyP>
     </>
   );
 }
